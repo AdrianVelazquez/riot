0cb589b5e709373811e3a3aaf08b2dabc919aef7
Adrian Velazquez <avelazquez585@gmail.com>
2019-07-01 16:35:19 -0400

    using promises and json to pull info from an api to show on the page

--- javascript/js/promises.js
+++ javascript/js/promises.js
@@ -21,4 +21,49 @@ promise2.then((resolve) => {
   console.log(resolve);
 });
 
-// fetch data from an api
+// fetch data from an api using ajax, method, url
+function getData(method, url){
+
+  return new Promise(function(resolve, reject){
+    let xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onload = function(){
+      if(this.status >= 200 && this.status < 300){
+        resolve(xhr.response)
+      }else{
+        reject({
+          status: this.status,
+          statusText: xhr.statusText
+        });
+      }
+    };
+
+    xhr.onerror = function(){
+      reject({
+        status: this.status,
+        statusText: xhr.statusText
+      });
+    }
+    xhr.send();
+  });
+}
+
+getData("GET", "https://jsonplaceholder.typicode.com/todos")
+  .then(function(data){
+    let todos = JSON.parse(data);
+    let output = "";
+
+    for(let todo of todos){
+      output += `
+        <li>
+          <h5>${todo.title}</h5>
+          <p>${todo.completed}</p>
+        </li>
+      `;
+    }
+    document.getElementById("template").innerHTML = output;
+    // console.log(data);
+  })
+  .catch(function(error){
+    console.log(error);
+  });
